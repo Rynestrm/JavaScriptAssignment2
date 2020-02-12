52b1b7e63d5d2d9d5d09b191513522230554d70d
Rynestrm <58361443+Rynestrm@users.noreply.github.com>
2020-02-12 18:15:02 -0500

    Add files via upload

---
+++ js/flowerGallery.js
@@ -0,0 +1,27 @@
+
+// JavaScript varaiable
+// targets all the images in any ul il
+let flower = document.querySelectorAll("ul li img");
+// targets all the images in the section > figure
+let topFlower = document.querySelector("section figure img");
+let fig = document.querySelector("figcaption");
+
+
+// Functions
+function largeImage(e) {
+  // targeting the event of what is clicked
+  let replaceImage = e.target.src;
+  let replaceAlt = e.target.alt;
+  console.log(replaceAlt);
+  // replacing the word small with the word large in the src path
+  let replaced = replaceImage.replace("small", "large");
+  let altReplaced = replaceAlt.replace("alt", "alt");
+  console.log(replaceAlt);
+  topFlower.src = replaced;
+  fig.innerHTML = replaceAlt;
+  // console.log(topFlower);
+}
+
+// on click events
+for (i=0; i < flower.length; i++)
+    flower[i].addEventListener('click', largeImage);
